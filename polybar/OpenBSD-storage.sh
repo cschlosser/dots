cc787ad5674683be6e83f8dde89a0b6f299ec5cd
Christoph <code@c.onl>
2025-11-10 20:48:28 +0100

    polybar: Only show partitions in OpenBSD if less than 50% free
    /home is always visible.

--- polybar/OpenBSD-storage.sh
+++ polybar/OpenBSD-storage.sh
@@ -8,7 +8,12 @@ while true; do
   while IFS= read -r LINE; do
     PART="$(awk '{print $1}' <<< $LINE)"
     PERC="$(awk '{print $5}' <<< $LINE)"
-    print_line ${PART: -1} $PERC
+    if [[ "${PART: -1}" != "d" && \
+      "${PERC:: -1}" -le 50 ]]; then
+        continue
+    fi
+    MNT="$(awk '{print $6}' <<< $LINE)"
+    print_line "${MNT}" "${PERC}"
   done <<< "$(df | grep '/dev/' | sort)"
   echo
   sleep 5
